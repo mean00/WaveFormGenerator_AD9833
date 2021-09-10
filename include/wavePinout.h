fcf93d1eafdfc493a8b47f27d79538952287ecc6
mean <fixounet@free.fr>
2021-09-10 07:28:54 +0200

    restructure

---
+++ include/wavePinout.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define AD9833_CS       PB11
+#define AD9833_SPI      1    // 2nd one !
+#define AD9833_AMP      PA0  // select amplified or not 
+
+#define SSD1306_I2C     0      // 1st one
+#define SSD1306_ADR     0x3c   
+#define SSD1306_SPEED  (400*1000)   
+
+
+#define ROTARY_LEFT     PA3 
+#define ROTARY_RIGHT    PA2 
+#define ROTARY_PUSH     PA1
+
---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+//
+//
+
+#include "lnArduino.h"
+#include "lnSPI.h"
+#include "simplerAD9833.h"
+#include "libraries/simplerAD9833/simplerAD9833.h"
+
+#include "RotaryEncoder.h"
+
+#include "libraries/simplerSSD1306/ssd1306_i2c_lnGd32vf103.h" // works also with gd32f1/Stm32f1
+#include "FreeSans7pt7b.h"
+
+#include "wavePinout.h"
+
+simplerAD9833 *ad;
+OLED_lnGd32 *ssd1306;
+lnRotary *rotary;
+
+
+void amp(bool onoff)
+{
+    digitalWrite(AD9833_AMP, onoff);
+}
+
+/**
+ * 
+ */
+void setup()
+{
+    pinMode(LN_SYSTEM_LED,OUTPUT);    
+    pinMode(AD9833_AMP,OUTPUT);    
+    
+    // ad9833
+    
+    digitalWrite(LN_SYSTEM_LED,0);
+    Logger("Starting AD9833...");
+    hwlnSPIClass *spi=new hwlnSPIClass(AD9833_SPI);
+    ad=new simplerAD9833(spi,AD9833_CS);
+    
+    // ssd now
+    lnI2C *i2c=new lnI2C(SSD1306_I2C,SSD1306_SPEED);
+    i2c->begin(SSD1306_ADR);
+    ssd1306=new OLED_lnGd32( *i2c, -1);
+    ssd1306->begin();
+    ssd1306->setFontFamily(&FreeSans7pt7b,&FreeSans7pt7b,&FreeSans7pt7b);
+    ssd1306->setFontSize(OLEDCore::SmallFont);
+
+    // rotary
+    rotary=new lnRotary(ROTARY_PUSH,ROTARY_LEFT,ROTARY_RIGHT);    
+}
+
+
+void loop()
+{
+    Logger("Entering main app...\n");
+    int onoff=0;
+    //
+    ssd1306->clrScr();
+    ssd1306->square(100,20,30,30,true);
+    ssd1306->print(2,40,"Demo");
+    ssd1306->update();
+
+    // use safe default
+    ad->setFrequency(10*1000);
+    ad->setWaveForm(simplerAD9833::Sine);
+    amp(true);
+    ad->enable();
+    
+    rotary->start();
+    
+    while(1)
+    {
+            amp(true);
+            xDelay(2000);
+            amp(false);
+            xDelay(2000);
+    }
+}
+// EOF
